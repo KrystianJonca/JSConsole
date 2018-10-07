d1ebe0089ae87ff545e5d1825e065d56483b3a27
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-07 18:24:30 +0200

    Linter commit
    Linter commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,84 +1,84 @@
-let output = document.getElementById("output");
-let input = document.getElementById("input");
-let toOutput;
+let output = document.getElementById('output')
+let input = document.getElementById('input')
+let toOutput
 
-document.onkeypress = function(event){
-    if (event.keyCode == 13) {
-        let inputVal = input.value.trim();
-        console.log(inputVal);
-        switch(inputVal){
-            case "!help":
-                help();
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-            break;
-            case "!clear":
-                clearConsole();
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-            break;
-            case "!time":
-                time();
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-            break;
-            case "!main":
-                main();
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-            break;
-            case "!contact":
-                contact();
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-            break;
-            case "!about":
-                about();
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-            break;
-            default:
-                Output("<span>Command not found!</span><br/>");
-                input.value = "";
-                input.value = input.value.replace(/\r?\n|\r/,"");
-        }
+document.onkeypress = function (event) {
+  if (event.keyCode == 13) {
+    let inputVal = input.value.trim()
+    console.log(inputVal)
+    switch (inputVal) {
+      case '!help':
+        help()
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
+        break
+      case '!clear':
+        clearConsole()
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
+        break
+      case '!time':
+        time()
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
+        break
+      case '!main':
+        main()
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
+        break
+      case '!contact':
+        contact()
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
+        break
+      case '!about':
+        about()
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
+        break
+      default:
+        Output('<span>Command not found!</span><br/>')
+        input.value = ''
+        input.value = input.value.replace(/\r?\n|\r/, '')
     }
+  }
 }
-function about(){
-    Output("<span>Hello! I'm a consol write in JavaScript, nice to meet you!</span><br/>");
+function about () {
+  Output("<span>Hello! I'm a consol write in JavaScript, nice to meet you!</span><br/>")
 }
-function contact(){
-    Output("<span>GitHub: https://github.com/KrystianJonca/</span><br/>");
+function contact () {
+  Output('<span>GitHub: https://github.com/KrystianJonca/</span><br/>')
 }
-function main(){
-    clearConsole();
-    Output('<span>Hello!</span><br/><span>Type "!help" to see available commands!</span><br/>');
+function main () {
+  clearConsole()
+  Output('<span>Hello!</span><br/><span>Type "!help" to see available commands!</span><br/>')
 }
-function time(){
-    let currentTime = new Date();
+function time () {
+  let currentTime = new Date()
 
-    let day = currentTime.getDate();
-    let month = currentTime.getMonth()+1;
-    let year = currentTime.getFullYear();
+  let day = currentTime.getDate()
+  let month = currentTime.getMonth() + 1
+  let year = currentTime.getFullYear()
 
-    let hours = currentTime.getHours();
-    let minutes = currentTime.getMinutes();
-    let seconds = currentTime.getSeconds();
-    let miliSeconds = currentTime.getMilliseconds();
+  let hours = currentTime.getHours()
+  let minutes = currentTime.getMinutes()
+  let seconds = currentTime.getSeconds()
+  let miliSeconds = currentTime.getMilliseconds()
 
-    Output("<span>"+day+"/"+month+"/"+year+"|"+hours+":"+minutes+":"+seconds+":"+miliSeconds+"</span><br/>");
+  Output('<span>' + day + '/' + month + '/' + year + '|' + hours + ':' + minutes + ':' + seconds + ':' + miliSeconds + '</span><br/>')
 }
-function help(){
-    let commandsList = ["Available commands:","!help","!clear","!time"
-    ,"!main","!contact","!about"];
-    for (let i = 0; i < commandsList.length; i++) {
-        let out = "<span>"+ commandsList[i] + "</span><br/>";
-        Output(out);
-    }
+function help () {
+  let commandsList = ['Available commands:', '!help', '!clear', '!time',
+    '!main', '!contact', '!about']
+  for (let i = 0; i < commandsList.length; i++) {
+    let out = '<span>' + commandsList[i] + '</span><br/>'
+    Output(out)
+  }
 }
-function clearConsole(){
-    output.innerText = "";
+function clearConsole () {
+  output.innerText = ''
 }
-function Output(data){
-    output.innerHTML += data;
+function Output (data) {
+  output.innerHTML += data
 }
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static('public'));
-app.set('view engine','ejs');
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
-app.get('/',(req,res) => {
-    res.render('index');
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+  res.status(404).render('404')
 })
 
-app.listen(precces.env.PORT || 8000)
+app.listen(8000, () => {
+  console.log('Listening on port 8000')
+})
